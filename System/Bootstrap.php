ebc52539b118530a1b1349c3257e9eb821b9d7d2
ClauWeb <claudiese00@gmail.com>
2021-01-18 02:18:37 +0100

    Delete System directory

--- System/Bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once(realpath(dirname(__FILE__)."/Resources/Autoload.php"));
-require_once(realpath(dirname(__FILE__)."/Resources/Configurations.php"));
-require_once(realpath(dirname(__FILE__)."/Resources/Constants.php"));
-require_once(realpath(dirname(__FILE__)."/Resources/Engine.php"));
-require_once(realpath(dirname(__FILE__)."/Resources/Route.php"));
-
-Route::run();
-?>
